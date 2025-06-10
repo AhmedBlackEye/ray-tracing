c0b20adc7a2551de51e550bdbc86aad1d7be2fa5
Ahmed Elsayed <ae924@ac.ic.uk>
2025-06-10 23:40:02 +0100

    Added vector operations

---
+++ vec3.h
@@ -0,0 +1,86 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <math.h>
+
+#define DBL_EPSILON 1e-6
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} Vec3;
+
+static inline Vec3 vec3_add(Vec3 v1, Vec3 v2) {
+    return (Vec3){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+}
+
+static inline Vec3 vec3_adds(Vec3 v1, double d) {
+    return (Vec3){v1.x + d, v1.y + d, v1.z + d};
+}
+
+static inline Vec3 vec3_sub(Vec3 v1, Vec3 v2) {
+    return (Vec3){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+}
+
+static inline Vec3 vec3_subs(Vec3 v1, double d) {
+    return (Vec3){v1.x - d, v1.y - d, v1.z - d};
+}
+
+static inline Vec3 vec3_mul(Vec3 v1, Vec3 v2) {
+    return (Vec3){v1.x * v2.x, v1.y * v2.y, v1.z * v2.z};
+}
+
+static inline Vec3 vec3_scale(Vec3 v1, double d) {
+    return (Vec3){v1.x * d, v1.y * d, v1.z * d};
+}
+
+static inline Vec3 vec3_div(Vec3 v1, Vec3 v2) {
+    return (Vec3){v1.x / v2.x, v1.y / v2.y, v1.z / v2.z};
+}
+
+static inline Vec3 vec3_divs(Vec3 v1, double d) {
+    return (Vec3){v1.x / d, v1.y / d, v1.z / d};
+}
+
+static inline double vec3_dot(Vec3 v1, Vec3 v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+static inline Vec3 vec3_cross(Vec3 v1, Vec3 v2) {
+    return (Vec3){
+        v1.y * v2.z - v1.z * v2.y,
+        v1.z * v2.x - v1.x * v2.z,
+        v1.x * v2.y - v1.y * v2.x
+    };
+}
+
+static inline double vec3_length(Vec3 v) {
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+static inline double vec3_length_squared(Vec3 v) {
+    return v.x * v.x + v.y * v.y + v.z * v.z;
+}
+
+static inline Vec3 vec3_normalize(Vec3 v) {
+    double len = vec3_length(v);
+    return len > 0.0 ? vec3_divs(v, len) : (Vec3){0, 0, 0};
+}
+
+static inline Vec3 vec3_zero(void) {
+    return (Vec3){0.0, 0.0, 0.0};
+}
+
+static inline Vec3 vec3_one(void) {
+    return (Vec3){1.0, 1.0, 1.0};
+}
+
+
+static inline int vec3_equal(Vec3 v1, Vec3 v2, double epsilon) {
+    return fabs(v1.x - v2.x) < epsilon && 
+           fabs(v1.y - v2.y) < epsilon && 
+           fabs(v1.z - v2.z) < epsilon;
+}
+
+#endif // VEC3_H
