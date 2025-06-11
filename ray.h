af0fde6c18dcc6f3c70a4963707c38572b2184e3
Ahmed Elsayed <ae924@ac.ic.uk>
2025-06-11 20:21:10 +0100

    Change ray member offset to origin to align with RT terminology

--- ray.h
+++ ray.h
@@ -4,12 +4,12 @@
 #include "vec3.h"
 
 typedef struct {
-  Vec3 offset;
+  Vec3 origin;
   Vec3 direction;
 } Ray;
 
 static inline Vec3 ray_at(Ray r, double pos) {
-  return vec3_add(vec3_scale(r.direction, pos), r.offset);
+  return vec3_add(vec3_scale(r.direction, pos), r.origin);
 }
 
 #endif // RAY_H
