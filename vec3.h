f741779a30e04a27ce74474e8631094b2aae622d
Ahmed Elsayed <ae924@ac.ic.uk>
2025-06-12 00:35:05 +0100

    Fixed function ordering in vec3

--- vec3.h
+++ vec3.h
@@ -58,7 +58,11 @@ static inline double vec3_length_squared(Vec3 v) {
   return v.x * v.x + v.y * v.y + v.z * v.z;
 }
 
-static inline Vec3 vec3_normalize(Vec3 v) {
+static inline Vec3 vec3_zero(void) { return (Vec3){0.0, 0.0, 0.0}; }
+
+static inline Vec3 vec3_one(void) { return (Vec3){1.0, 1.0, 1.0}; }
+
+static inline Vec3 vec3_normalized(Vec3 v) {
   double len = vec3_length(v);
   return len > 0.0 ? vec3_divs(v, len) : vec3_zero();
 }
@@ -67,10 +71,6 @@ static inline void vec3_normalize(Vec3 *v) {
   double len = vec3_length(*v);
   *v = len > 0.0 ? vec3_divs(*v, len) : vec3_zero();
 }
-
-static inline Vec3 vec3_zero(void) { return (Vec3){0.0, 0.0, 0.0}; }
-
-static inline Vec3 vec3_one(void) { return (Vec3){1.0, 1.0, 1.0}; }
 
 static inline int vec3_equal(Vec3 v1, Vec3 v2) {
   return fabs(v1.x - v2.x) < DBL_EPSILON && fabs(v1.y - v2.y) < DBL_EPSILON &&
