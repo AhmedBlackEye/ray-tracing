284ef1dc01a7e5966a5a83530bf0a36428f89dee
Joshua Jones <jdj24@ic.ac.uk>
2025-06-13 16:48:00 +0100

    Removed magic numbers and bug fixed scene parser

--- core/scene_parser.c
+++ core/scene_parser.c
@@ -15,7 +15,7 @@ static int tokenize(char *line, char *tokens[]) {
     char *saveptr = NULL;
     char *token = strtok_r(line, " \t\n", &saveptr);
     
-    while (token && count < MAX_TOKENS) {
+    while (token && num_toks < MAX_TOKENS) {
         tokens[num_toks++] = token;
         token = strtok_r(NULL, " \t\n", &saveptr);
     }
@@ -29,7 +29,7 @@ void parse_scene(const char *filename, DynArray *hittable_world, FILE *out_file)
 
     ParserState state = TOPLEVEL_STATE;
 
-    char line[MAX_LINE];
+    char line[MAX_LINE_LENGTH];
     char *tokens[MAX_TOKENS];
 
     Vec3 center;
@@ -43,7 +43,7 @@ void parse_scene(const char *filename, DynArray *hittable_world, FILE *out_file)
     double aspect_ratio;
     int width;
 
-    while (fgets(line, MAX_LINE, file)) {
+    while (fgets(line, MAX_LINE_LENGTH, file)) {
         if (line[0] == '\n') {
             continue;
         }
@@ -54,10 +54,10 @@ void parse_scene(const char *filename, DynArray *hittable_world, FILE *out_file)
                     dynarray_push(hittable_world, sphere_create(center, radius));
                     break;
                 case PLANE_STATE:
-                    dynarray_push(world, plane_create(point, normal));
+                    dynarray_push(hittable_world, plane_create(point, normal));
                     break;
                 case CAMERA_STATE:
-                    Camera cam = camera_make(WIDTH, ASPECT_RATIO);
+                    Camera cam = camera_make(width, aspect_ratio);
                     camera_render(&cam, hittable_world, out_file);
                     break;
             }
@@ -68,7 +68,7 @@ void parse_scene(const char *filename, DynArray *hittable_world, FILE *out_file)
         int num_toks = tokenize(line, tokens);
         if (num_toks == 0) continue;
 
-        if (state == STATE_TOPLEVEL) {
+        if (state == TOPLEVEL_STATE) {
             if (strcmp(tokens[0], "camera") == 0) {
                 state = CAMERA_STATE;
             }
@@ -83,38 +83,38 @@ void parse_scene(const char *filename, DynArray *hittable_world, FILE *out_file)
             switch (state) {
                 case SPHERE_STATE:
                     if (strcmp(tokens[0], "center") == 0 && num_toks == 4) {
-                        position.x = atof(tokens[1]);
-                        position.y = atof(tokens[2]);
-                        position.z = atof(tokens[3]);
+                        center.x = atof(tokens[1]);
+                        center.y = atof(tokens[2]);
+                        center.z = atof(tokens[3]);
                     }
                     else if (strcmp(tokens[0], "radius") == 0 && num_toks == 2) {
                         radius = atof(tokens[1]);
                     }
                     break;
-                case STATE_PLANE:
-                    if (strcmp(tokens[0], "point") == 0 && count == 4) {
+                case PLANE_STATE:
+                    if (strcmp(tokens[0], "point") == 0 && num_toks == 4) {
                         point.x = atof(tokens[1]);
                         point.y = atof(tokens[2]);
                         point.z = atof(tokens[3]);
                     }
-                    else if (strcmp(tokens[0], "normal") == 0 && count == 4) {
+                    else if (strcmp(tokens[0], "normal") == 0 && num_toks == 4) {
                         normal.x = atof(tokens[1]);
                         normal.y = atof(tokens[2]);
                         normal.z = atof(tokens[3]);
                     }
                     break;
                 case CAMERA_STATE:
-                    if (strcmp(tokens[0], "aspect_ratio") == 0 && count == 3) {
+                    if (strcmp(tokens[0], "aspect_ratio") == 0 && num_toks == 3) {
                         aspect_ratio_width = atof(tokens[1]);
                         aspect_ratio_height = atof(tokens[2]);
                         aspect_ratio = aspect_ratio_width / aspect_ratio_height;
                     }
-                    
+                    else if (strcmp(tokens[0], "width") == 0 && num_toks == 2) {
+                        width = atoi(tokens[1]);
+                    }
             }
 
         }
-        fclose(file);
     }
-
-    ParserState state = STATE_TOPLEVEL;
+    fclose(file);
 }
