1d2cd9375b0b5ac30a5c2afda603738957786227
Joshua Jones <jdj24@ic.ac.uk>
2025-06-13 20:04:26 +0100

    Added quadrilateral to scene_parser

--- main.c
+++ main.c
@@ -38,11 +38,6 @@ int main(int argc, char **argv) {
   DynArray *hittable_world =
       dynarray_create(2, (GPrintFn)hittable_print, (GDestroyFn)hittable_destroy);
 
-  Vec3 Q = { -5.0, 0.0, -1.5 };
-    Vec3 u = { 10.0, 0.0,  0.0 };
-    Vec3 v = {  0.0, 10.0, 0.0 };
-    dynarray_push(hittable_world, quad_create(Q, u, v));
-
   parse_scene(argv[1], hittable_world, out_file);    
 
   dynarray_destroy(hittable_world);
