b048218ced0f9af4c3bb2223cb673266693999a7
Joshua Jones <jdj24@ic.ac.uk>
2025-06-16 15:59:48 +0100

    Completed diffuse_light.c

--- material/diffuse_light.c
+++ material/diffuse_light.c
@@ -1,10 +1,16 @@
 #include "diffuse_light.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
-typedef struct {
+typedef struct DiffuseLight {
     Color emit_color; 
-} DiffuseLightData;
+} DiffuseLight;
+
+static bool diffuse_light_scatter(Material *mat, Ray ray_in, HitRecord *rec,
+                                  Color *attenuation, Ray *scattered) {
+    return false;
+}
 
 static void diffuse_light_destroy(void *self) {
   assert(self != NULL);
@@ -14,12 +20,39 @@ static void diffuse_light_destroy(void *self) {
   free(self);
 }
 
-void dielectric_print(const Material *self) {
-  if (self == NULL || self->type != MATERIAL_LAMBERTIAN) {
-    printf("Dielectric: Invalid or NULL\n");
+void diffuse_light_print(const Material *self) {
+  if (self == NULL || self->type != MATERIAL_DIFFUSE_LIGHT) {
+    printf("Diffuse Light: Invalid or NULL\n");
     return;
   }
-  const Dielectric *dielectric = (const Dielectric *)self->data;
-  printf("Dielectric { refractive_index: %.3f}\n",
-         dielectric->refraction_index);
+  const DiffuseLight *diff_l = (const DiffuseLight *)self->data;
+  Color emit_color = diff_l->emit_color;
+  printf("Diffuse Light { emit colour: %.3f %.3f %.3f}\n", emit_color.x, emit_color.y, emit_color.z);
+}
+
+Color diffuse_light_emitted(Material *self, double u, double v, const Vec3 *p) {
+    if (!self || self->type != MATERIAL_DIFFUSE_LIGHT) {
+        return vec3_zero();
+    }
+    
+    DiffuseLight *data = (DiffuseLight*)self->data;
+    return data->emit_color;
+}
+
+Material *diffuse_light_create(Color emit_color) {
+  Material *mat = malloc(sizeof(struct Material));
+  assert(mat != NULL);
+
+  DiffuseLight *diff_l = malloc(sizeof(struct DiffuseLight));
+  assert(diff_l != NULL);
+
+  diff_l->emit_color = emit_color;
+
+  mat->type = MATERIAL_DIFFUSE_LIGHT;
+  mat->scatter = (ScatterFn)diffuse_light_scatter;
+  mat->destroy = (MaterialDestroyFn)diffuse_light_destroy;
+  mat->emitted = diffuse_light_emitted;
+  mat->data = diff_l;
+
+  return mat;
 }
