7ccfb08f671a3b80228e9aec9afbc31faf6770d6
Joshua Jones <jdj24@ic.ac.uk>
2025-06-19 16:23:56 +0100

    Fixed parser

--- main.c
+++ main.c
@@ -42,31 +42,6 @@ int main(int argc, char **argv) {
     Camera cam;
     parse_scene(argv[1], &scene, &cam);
 
-    Material *red = scene_add_material(&scene, lambertian_create((Vec3){0.8, 0.2, 0.2}));
-    Material *blue = scene_add_material(&scene, lambertian_create((Vec3){0.2, 0.2, 0.8}));
-
-    // Normal sphere (red)
-    Hittable *normal_sphere = sphere_create((Vec3){-2, 0, -5}, 1.0, red);
-    scene_add_obj(&scene, normal_sphere);
-
-    // Rotated sphere (blue) - should look identical since spheres are symmetric
-    Hittable *sphere_to_rotate = sphere_create((Vec3){2, 0, -5}, 1.0, blue);  
-    Hittable *rotated_sphere = rotate_y_create(sphere_to_rotate, 45.0);
-    scene_add_obj(&scene, rotated_sphere);
-
-    Vec3 a = { -1.0, 0.0, -1.0 };  
-    Vec3 b = {  1.0, 2.0,  1.0 };
-
-    DynArray *box_sides = box_create(a, b, box_mat);          // 6 quads
-
-    for (size_t i = 0; i < dynarray_size(box_sides); ++i) {
-        Hittable *face = (Hittable *)dynarray_get(box_sides, i);
-        scene_add_obj(scene, face);                           // <- real use
-    }
-
-    /* destroy only the container; the quads now live in the scene   */
-    dynarray_destroy(box_sides);
-    
     camera_render(&cam, scene.objects, out_file);
     scene_destroy(&scene);
     fclose(out_file);
